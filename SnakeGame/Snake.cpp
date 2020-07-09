200d172bdb90e153a2b6efe5add3a9d9443302cd
= <=>
2020-07-09 21:32:32 +0700

    Add project files.

---
+++ SnakeGame/Snake.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+#include<math.h>
+#include<vector>
+#include<ctime>
+#include<cstring>
+#include<windows.h>
+using namespace std;
+
+struct DIEM {};
+/*Biểu diễn tọa độ các thành phần
+gồm các hàm khởi tạo, phép kiểm tra trùng, lấy hiệu 2 điểm.*/
+struct RAN {};
+/*Biểu diễn rắn
+gồm các hàm định hướng lúc di chuyển, xuất hiện, di chuyển và ăn mồi*/
+struct MOI {};
+/*Biểu diễn vị trí mồi
+gồm các hàm xuất hiện, kiểm tra vị trí của mồi khi xuất hiện, bị rắn ăn*/
+struct KHUNG {};
+/*Hiển thị khung trò chơi khi trò chơi bắt đầu
+bao gồm cả hàm vẽ khung, vẽ rắn, vẽ mồi, xóa rắn, kết thúc game, và giao diện hiện điểm số*/
+
+// Thêm Một hàm để xóa con trỏ trong màn hình console
+void Nocursortype()
+{}
+// Hàm nhập tùy chọn khi vào game
+void NhapCacYeuCau()
+{}
+int main()
+{
+	return 0;
+}
